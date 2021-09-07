bfaca7c79ac55d751eae560532070d30fb6f7148
Danilo Novaes <danilo.novaes@hotmail.com.br>
2021-09-06 22:39:01 -0300

    arquivo
    migration da fatura

---
+++ d-conta-corrente/src/main/resources/db/migration/V5__Create_Table_fatura.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE `fatura` (
+  `id` INT(10) AUTO_INCREMENT PRIMARY KEY,
+  `saldo` decimal(65,2) NOT NULL,
+  `tipo` CHAR(20) NOT NULL,
+  `doc` CHAR(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ d-conta-corrente/src/main/resources/db/migration/V6__Populate_Table_fatura.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO `fatura` (`saldo`, `tipo`, `doc`) VALUES
+                     (10000.00, 'fatura', '1'),
+					 (21222.22, 'fatura', '2'),
+					 (1234.0, 'fatura', '3'),
+					 (65443.00, 'fatura', '4'),
+					 (2134454.00, 'fatura', '5'),
+					 (423423.43, 'fatura', '6'),
+					 (66556565.99, 'fatura', '7');
